c4c8322ed7984fa69c513309853ca078bbf92d2f
FSOFT.FPT.VN\VyLT6 <vylt6@fsoft.com.vn>
2022-10-05 17:02:03 +0700

    lunar

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+<script>
+import DatePicker from './components/DatePicker.vue';
+
+export default {
+  components: {
+    DatePicker
+  },
+
+  data() {
+    return {
+      range: null
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div id="app">
+    <DatePicker @get-range="(range) => this.range = range"></DatePicker>
+
+    <div v-if="range != null">
+      The date range is: {{range}}
+    </div>
+
+    
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/BarChart.vue
@@ -0,0 +1,195 @@
+<script>
+    export default {
+        data: {
+            sample:
+                [
+                    {
+                        "time": 1662310800000,
+                        "label": "05/09",
+                        "dataset1": 24,
+                        "dataset2": 10
+                    },
+                    {
+                        "time": 1662397200000,
+                        "label": "06/09",
+                        "dataset1": 79,
+                        "dataset2": 14
+                    },
+                    {
+                        "time": 1662483600000,
+                        "label": "07/09",
+                        "dataset1": 53,
+                        "dataset2": 87
+                    },
+                    {
+                        "time": 1662570000000,
+                        "label": "08/09",
+                        "dataset1": 20,
+                        "dataset2": 18
+                    },
+                    {
+                        "time": 1662656400000,
+                        "label": "09/09",
+                        "dataset1": 41,
+                        "dataset2": 36
+                    },
+                    {
+                        "time": 1662742800000,
+                        "label": "10/09",
+                        "dataset1": 24,
+                        "dataset2": 86
+                    },
+                    {
+                        "time": 1662829200000,
+                        "label": "11/09",
+                        "dataset1": 26,
+                        "dataset2": 28
+                    },
+                    {
+                        "time": 1662915600000,
+                        "label": "12/09",
+                        "dataset1": 28,
+                        "dataset2": 83
+                    },
+                    {
+                        "time": 1663002000000,
+                        "label": "13/09",
+                        "dataset1": 90,
+                        "dataset2": 68
+                    },
+                    {
+                        "time": 1663088400000,
+                        "label": "14/09",
+                        "dataset1": 7,
+                        "dataset2": 29
+                    },
+                    {
+                        "time": 1663174800000,
+                        "label": "15/09",
+                        "dataset1": 0,
+                        "dataset2": 67
+                    },
+                    {
+                        "time": 1663261200000,
+                        "label": "16/09",
+                        "dataset1": 82,
+                        "dataset2": 99
+                    },
+                    {
+                        "time": 1663347600000,
+                        "label": "17/09",
+                        "dataset1": 41,
+                        "dataset2": 42
+                    },
+                    {
+                        "time": 1663434000000,
+                        "label": "18/09",
+                        "dataset1": 24,
+                        "dataset2": 38
+                    },
+                    {
+                        "time": 1663520400000,
+                        "label": "19/09",
+                        "dataset1": 15,
+                        "dataset2": 88
+                    },
+                    {
+                        "time": 1663606800000,
+                        "label": "20/09",
+                        "dataset1": 48,
+                        "dataset2": 93
+                    },
+                    {
+                        "time": 1663693200000,
+                        "label": "21/09",
+                        "dataset1": 71,
+                        "dataset2": 96
+                    },
+                    {
+                        "time": 1663779600000,
+                        "label": "22/09",
+                        "dataset1": 34,
+                        "dataset2": 96
+                    },
+                    {
+                        "time": 1663866000000,
+                        "label": "23/09",
+                        "dataset1": 1,
+                        "dataset2": 69
+                    },
+                    {
+                        "time": 1663952400000,
+                        "label": "24/09",
+                        "dataset1": 89,
+                        "dataset2": 28
+                    },
+                    {
+                        "time": 1664038800000,
+                        "label": "25/09",
+                        "dataset1": 60,
+                        "dataset2": 71
+                    },
+                    {
+                        "time": 1664125200000,
+                        "label": "26/09",
+                        "dataset1": 34,
+                        "dataset2": 99
+                    },
+                    {
+                        "time": 1664211600000,
+                        "label": "27/09",
+                        "dataset1": 20,
+                        "dataset2": 2
+                    },
+                    {
+                        "time": 1664298000000,
+                        "label": "28/09",
+                        "dataset1": 24,
+                        "dataset2": 23
+                    },
+                    {
+                        "time": 1664384400000,
+                        "label": "29/09",
+                        "dataset1": 6,
+                        "dataset2": 38
+                    },
+                    {
+                        "time": 1664470800000,
+                        "label": "30/09",
+                        "dataset1": 89,
+                        "dataset2": 3
+                    },
+                    {
+                        "time": 1664557200000,
+                        "label": "01/10",
+                        "dataset1": 11,
+                        "dataset2": 48
+                    },
+                    {
+                        "time": 1664643600000,
+                        "label": "02/10",
+                        "dataset1": 86,
+                        "dataset2": 68
+                    },
+                    {
+                        "time": 1664730000000,
+                        "label": "03/10",
+                        "dataset1": 92,
+                        "dataset2": 58
+                    },
+                    {
+                        "time": 1664816400000,
+                        "label": "04/10",
+                        "dataset1": 82,
+                        "dataset2": 72
+                    },
+                    {
+                        "time": 1664902800000,
+                        "label": "05/10",
+                        "dataset1": 78,
+                        "dataset2": 68
+                    }
+                ]
+        }
+    }
+</script>
